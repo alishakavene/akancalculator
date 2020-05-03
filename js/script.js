f28fdd6a384aeb5dea2ac7a9200481c41005eab7
alishakavene <64075529+alishakavene@users.noreply.github.com>
2020-05-03 08:46:13 +0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function getAkanName () {
+  let yearOfBirth = document.getElementById("year-input").value;
+  let monthOfBirth = Number(document.getElementById("month-input").value);
+  let dayOfBirth = Number(document.getElementById("day-input").value);
+  let genders = document.getElementsByName("gender");
+
+  // function to get gender
+  function getGender () {
+    for (let gender of genders){
+      if (gender.checked){
+        return gender.value;
+      }
+    }
+  }
+
+  let myGenderValue = getGender();
+  console.log(myGenderValue);
+
+  // validation functions
+  function monthValidator () {
+    if (monthOfBirth < 1 || monthOfBirth > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
